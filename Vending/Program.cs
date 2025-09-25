b4817dce6bbe44e86ae5901ba8a67c2563109e5f
LatverianDOOMBOT <drgamesbg@gmail.com>
2025-09-25 10:41:32 +0300

    added comments as answers to q's

--- Vending/Program.cs
+++ Vending/Program.cs
@@ -5,7 +5,7 @@ vendingPairs.Add(6, "fuzetea");
 vendingPairs.Add(7, "iced tea");
 vendingPairs.Add(76, "cold tea");
 int vendingNum = 3;
-bool isRunning = true;
+bool isRunning = true; // bool s koito da ne e bezkraino, break breakva loopa ppc.
 while (isRunning)
 {
     Console.WriteLine("Choose product");
@@ -23,12 +23,12 @@ while (isRunning)
             if (vendingPairs.ContainsKey(vendingNum))
             {
                 
-                Console.WriteLine("You have chosen " + vendingPairs[vendingNum]);
+                Console.WriteLine("You have chosen " + vendingPairs[vendingNum]); //nqmashe nuzhda da se prisvoqva obache burzah da dovursha programata ne sum mislil osobeno mnogo tbh
             }
             else
             {
                 Console.WriteLine("Invalid product");
-                continue;
+                continue; // continue produlzhava loopa
             }
 
         }
